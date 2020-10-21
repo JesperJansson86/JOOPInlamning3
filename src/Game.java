d18137790a567a64a0bbb183e548e1473b335097
Jesper Jansson <jesper.jansson@yh.nackademin.se>
2020-10-21 15:11:05 +0200

    Startup- Logicbranch.

--- src/Game.java
+++ src/Game.java
@@ -6,25 +6,26 @@ public class Game {
 
 
     }
-public static void initiate(){
-    Position one = new Position(1);
-    Position two = new Position(2);
-    Position three = new Position(3);
-    Position four = new Position(4);
-    Position five = new Position(6);
-    Position six = new Position(6);
-    Position seven = new Position(7);
-    Position eight = new Position(8);
-    Position nine = new Position(9);
-    Position ten = new Position(10);
-    Position eleven = new Position(11);
-    Position twelve = new Position(12);
-    Position thirteen = new Position(13);
-    Position fourteen = new Position(14);
-    Position fiftheen = new Position(15);
-    Position sixteen = new Position(16);
-    sixteen.free=true;
-}
+
+    public static void initiate() {
+        Position one = new Position(1);
+        Position two = new Position(2);
+        Position three = new Position(3);
+        Position four = new Position(4);
+        Position five = new Position(6);
+        Position six = new Position(6);
+        Position seven = new Position(7);
+        Position eight = new Position(8);
+        Position nine = new Position(9);
+        Position ten = new Position(10);
+        Position eleven = new Position(11);
+        Position twelve = new Position(12);
+        Position thirteen = new Position(13);
+        Position fourteen = new Position(14);
+        Position fiftheen = new Position(15);
+        Position sixteen = new Position(16);
+        sixteen.free = true;
+    }
 
 }
 
