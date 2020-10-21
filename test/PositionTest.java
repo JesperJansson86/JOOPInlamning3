90fa6135d041e488d1aafa6ac0ea369527340060
kdhedman <kenth.david.hedman@gmail.com>
2020-10-21 14:58:12 +0200

    testDirectory added

---
+++ test/PositionTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by David Hedman <br>
+ * Date: 2020-10-21 <br>
+ * Time: 14:57 <br>
+ * Project: Sprint3 <br>
+ * Copyright: Nackademin <br>
+ */
+class PositionTest {
+
+}
