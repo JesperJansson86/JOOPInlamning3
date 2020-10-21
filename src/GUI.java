140983627eba7ea73c92184962f4216b914a37cd
kdhedman <kenth.david.hedman@gmail.com>
2020-10-21 16:50:00 +0200

    Basic GUI done

--- src/GUI.java
+++ src/GUI.java
@@ -1,10 +1,56 @@
-/**
- * Created by David Hedman <br>
- * Date: 2020-10-21 <br>
- * Time: 15:10 <br>
- * Project: Sprint3 <br>
- * Copyright: Nackademin <br>
- */
-public class GUI {
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 
+public class GUI extends JFrame {
+    private JPanel pMain = new JPanel();
+    private JPanel pCenter = new JPanel();
+    private JPanel pSouth = new JPanel();
+    private JPanel pNorth = new JPanel();
+    private JPanel tileGrid = new JPanel();
+    private JButton buttonNewGame = new JButton("New Game");
+    private List<JButton> tiles = new ArrayList<>();
+    private List<Image> icons = new ArrayList<Image>();
+
+
+
+    public GUI(){
+
+        setLayout(new FlowLayout());
+        add(pMain);
+        pMain.setLayout(new BorderLayout());
+
+        pMain.add(pNorth, BorderLayout.NORTH);
+        pNorth.add(new JLabel("Arrange the tiles in numerical order"));
+
+        pMain.add(pCenter, BorderLayout.CENTER);
+        pCenter.setLayout(new GridLayout(4,4));
+        for(int i = 1; i <= 16; i++){
+            JButton tile = new JButton(i + "");
+            tile.setPreferredSize(new Dimension(80,80));
+            tiles.add(tile);
+            //tile.addActionListener(l -> moveTile());
+        }
+        for(JButton tile : tiles){
+            pCenter.add(tile);
+        }
+        //startNewGame();
+
+        pMain.add(pSouth, BorderLayout.SOUTH);
+        pSouth.add(buttonNewGame);
+        buttonNewGame.setPreferredSize(new Dimension(320,20));
+        //buttonNewGame.addActionListener(l -> startNewGame());
+
+        setTitle("15 Game");
+        setVisible(true);
+        setLocationRelativeTo(null);
+        pack();
+        setMinimumSize(this.getSize());
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        GUI gui = new GUI();
+    }
 }
